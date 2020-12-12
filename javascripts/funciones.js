a615ec7ac70c231acc2d187f44666755e4fe8b71
Jairo Buendía <jairobuendialopez@gmail.com>
2020-12-12 23:49:11 +0100

    v1

---
+++ javascripts/funciones.js
@@ -0,0 +1,11 @@
+/* funciones javascript para la landing page de Sistemas Informáticos */
+/*barra arriba responsive */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
